5cca5d0f9b3b5bd78fc87e44df3bbed511091186
Wanley Rooby Alexis <alexroob96@gmail.com>
2026-06-10 15:13:29 -0300

    feat: adiciona entidade User e migration inicial

---
+++ src/main/java/com/wanroo/finance/entity/Role.java
@@ -0,0 +1,6 @@
+package com.wanroo.finance.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/wanroo/finance/entity/User.java
@@ -0,0 +1,41 @@
+package com.wanroo.finance.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+}
